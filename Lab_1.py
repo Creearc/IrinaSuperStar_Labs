78349ae6f81478c0b1f8bdab0a0880d6873feff9
rinarigel <bobrovairina18@gmail.com>
2019-09-15 23:27:00 +0500

    add new data

--- Lab_1.py
+++ Lab_1.py
@@ -1,9 +1,17 @@
 Book = []
-Book.append( {"Room_number": 105, "Surname": 'Назарова',
-     "Arrival_time": (22, 5), "Departure_time": (8, 10),
-     "Guest_info": 'Рассохин', "Document": 'Трудовая книжка'})
-Book.append( {"Room_number": 603, "Surname": 'Баринов',
-     "Arrival_time": (20, 50), "Departure_time": (23, 0),
-     "Guest_info": 'Мартонова', "Document": 'пропуск'})
+Book.append({"Room_number": 105, "Surname": 'Назарова',
+             "Arrival_time": (22, 5), "Departure_time": (8, 10),
+             "Guest_info": 'Рассохин', "Document": 'Трудовая книжка'})
+Book.append({"Room_number": 603, "Surname": 'Баринов',
+             "Arrival_time": (20, 50), "Departure_time": (23, 0),
+             "Guest_info": 'Мартонова', "Document": 'пропуск'})
+Book.append({"Room_number": 304, "Surname": 'Яковлева',
+             "Arrival_time": (21, 10), "Departure_time": (22, 15),
+             "Guest_info": 'Балезина', "Document": 'Водительские права'})
+Book.append({"Room_number": 407, "Surname": 'Боронников',
+             "Arrival_time": (19, 45), "Departure_time": (22, 40),
+             "Guest_info": 'Малышев', "Document": 'снилс'})
+Book.append({"Room_number": 404, "Surname": 'Найденов',
+             "Arrival_time": (21, 30), "Departure_time": (23, 00),
+             "Guest_info": 'Морозов', "Document": 'паспорт'})
 print(Book)
-
