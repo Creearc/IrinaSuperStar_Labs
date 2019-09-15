90c1cbb215be8a74e313d3c1aadfd78197b898ba
Alexandr <knxandr@rambler.ru>
2019-09-15 22:37:16 +0500

    Create Lab_1.py
    Основы основ...

---
+++ Lab_1.py
@@ -0,0 +1,9 @@
+Book = []
+Book.append( {"Room_number": 105, "Surname": 'Назарова',
+     "Arrival_time": (22, 5), "Departure_time": (8, 10),
+     "Guest_info": 'Рассохин', "Document": 'Трудовая книжка'})
+Book.append( {"Room_number": 603, "Surname": 'Баринов',
+     "Arrival_time": (20, 50), "Departure_time": (23, 0),
+     "Guest_info": 'Мартонова', "Document": 'пропуск'})
+print(Book)
+
