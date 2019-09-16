c8c8820dae3fb6a6a751e6289e84ad1966df31ae
Alexandr <knxandr@rambler.ru>
2019-09-16 23:08:57 +0500

    Переместил в папку Lab_1

--- Lab_1/Lab_1.py
+++ Lab_1/Lab_1.py
@@ -12,6 +12,6 @@ Book.append({"Room_number": 407, "Surname": 'Боронников',
              "Arrival_time": (19, 45), "Departure_time": (22, 40),
              "Guest_info": 'Малышев', "Document": 'снилс'})
 Book.append({"Room_number": 404, "Surname": 'Найденов',
-             "Arrival_time": (21, 30), "Departure_time": (23, 00),
+             "Arrival_time": (21, 30), "Departure_time": (23, 0),
              "Guest_info": 'Морозов', "Document": 'паспорт'})
 print(Book)
