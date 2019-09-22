3f7a0f2949732387728410808ec9172df328da7f
Alexandr <knxandr@rambler.ru>
2019-09-22 21:33:35 +0500

    Модули

---
+++ Lab_1/book.py
@@ -0,0 +1,8 @@
+import names as nm
+
+book = []
+
+book.append({nm.ROOM_NUMBER: 105, nm.SURNAME: 'Назарова', 
+             nm.ARRIVAL_TIME: (22, 5), nm.DEPARTURE_TIME: (8, 10), 
+             nm.GUEST_INFO: 'Рассохин', nm.DOCUMENT: 'Трудовая книжка'})
+
---
+++ Lab_1/names.py
@@ -0,0 +1,7 @@
+ROOM_NUMBER = "Номер комнаты"
+SURNAME = "Фамилия"
+ARRIVAL_TIME = "Время прибытия"
+DEPARTURE_TIME = "Время убытия"
+GUEST_INFO = "ФИО гостя"
+DOCUMENT = "Документ"
+
