9e0c2bb698c5db07c9c4edc95337819a2d2619a9
rinarigel <bobrovairina18@gmail.com>
2019-09-22 22:13:09 +0500

    data added (PEP8)

--- Lab_1/book.py
+++ Lab_1/book.py
@@ -2,7 +2,19 @@ import names as nm
 
 book = []
 
-book.append({nm.ROOM_NUMBER: 105, nm.SURNAME: 'Назарова', 
-             nm.ARRIVAL_TIME: (22, 5), nm.DEPARTURE_TIME: (8, 10), 
+book.append({nm.ROOM_NUMBER: 105, nm.SURNAME: 'Назарова',
+             nm.ARRIVAL_TIME: (22, 5), nm.DEPARTURE_TIME: (8, 10),
              nm.GUEST_INFO: 'Рассохин', nm.DOCUMENT: 'Трудовая книжка'})
+book.append({nm.ROOM_NUMBER: 603, nm.SURNAME: 'Баринов',
+             nm.ARRIVAL_TIME: (20, 50), nm.DEPARTURE_TIME: (23, 0),
+             nm.GUEST_INFO: 'Мартонова', nm.DOCUMENT: 'пропуск'})
+book.append({nm.ROOM_NUMBER: 304, nm.SURNAME: 'Яковлева',
+             nm.ARRIVAL_TIME: (21, 10), nm.DEPARTURE_TIME: (22, 15),
+             nm.GUEST_INFO: 'Балезина', nm.DOCUMENT: 'Водительские права'})
+book.append({nm.ROOM_NUMBER: 407, nm.SURNAME: 'Боронников',
+             nm.ARRIVAL_TIME: (19, 45), nm.DEPARTURE_TIME: (22, 40),
+             nm.GUEST_INFO: 'Малышев', nm.DOCUMENT: 'снилс'})
+book.append({nm.ROOM_NUMBER: 404, nm.SURNAME: 'Найденов',
+             nm.ARRIVAL_TIME: (21, 30), nm.DEPARTURE_TIME: (23, 0),
+             nm.GUEST_INFO: 'Морозов', nm.DOCUMENT: 'паспорт'})
 
