1aa7c02e3c1fae93ce593a38f28de77ee03cf0fc
rinarigel <49832353+rinarigel@users.noreply.github.com>
2019-10-08 12:37:45 +0500

    Update lab_3.py

--- lab_3.py
+++ lab_3.py
@@ -32,9 +32,11 @@ class Room(object):
 
 def test(data, space):
     for s in data.__dir__():
-        if s == "__class__": break
+        if s == "__class__":
+            break
         attr = data.__getattribute__(s).__repr__()
-        if attr[1:10] == "built-in" or attr[1:10] == "method-wr": break
+        if attr[1:10] == "built-in" or attr[1:10] == "method-wr":
+            break
         print("%s%s" % (space, attr))
         test(data.__getattribute__(s), space + "  ")
 
